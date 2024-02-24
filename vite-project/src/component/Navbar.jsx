8817f903705a1c2e310c358e116b343d63b4f52c
amit om kashyap <amit.ece.21@nitap.ac.in>
2024-02-25 01:12:54 +0530

    day1

--- vite-project/src/component/Navbar.jsx
+++ vite-project/src/component/Navbar.jsx
@@ -1,12 +1,20 @@
+import "./css/Navbar.css";
 function navbar(){
-    return(<div>
+    return(<div className="container">
+        <div className="left">
+        <a>Amit Om Kashyap</a>
+        </div>
+
+        <div className="right">
         <ul className="menu">
         <li><a href="#home">Home</a></li>
         <li><a href="#skills">Skills</a></li>
         <li><a href="#projects">Projects</a></li>
         <li><a href="#about">About</a></li>
-      </ul>
-      </div>
+        </ul>
+        </div>
+        
+    </div>
     )
 }
 export default navbar;
