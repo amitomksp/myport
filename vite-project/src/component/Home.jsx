38c5f20ca087a83640c1a96a3a63c039deb2f166
amit om kashyap <amit.ece.21@nitap.ac.in>
2024-02-25 01:33:00 +0530

    day1

---
+++ vite-project/src/component/Home.jsx
@@ -0,0 +1,13 @@
+import "./css/Home.css"
+function Home(){
+    (    <div className="container">
+        <div className="left">
+           
+        </div>
+        <div className="right">
+
+        </div>
+        </div>
+    )
+}
+export default Home;
