44346ff90d874d4d6f16a51b258280ad1e5510a8
amit om kashyap <amit.ece.21@nitap.ac.in>
2024-02-29 19:34:27 +0530

    Day6

--- vite-project/src/component/Home.jsx
+++ vite-project/src/component/Home.jsx
@@ -1,28 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./css/Home.css"
-import profile from "../images/profile_pic.jpg";
-function Home(){
-   return (    <div className="homecontainer background-image">
-        <div className="homeleft">
-            <div className="profile-circle">
-            <img src={profile}  alt="" />
-              </div>
-              <div className="name">
-                <div className='title'>Amit om kashyap</div>
-                <div className='description'> Full-stack web developer with knee interest in machine learning and artificial intelligency </div>
-              </div>
-           
-          
-           
-        </div>
-        <div className="homeright ">
-            <h2>Hello!</h2>
-            <a>I'm Amit kumar, a software developer and engineer who likes making projects from scratch and learning new technologies. I'm a electronics and communication engineering student pursuing B. Tech. from NIT Arunachal Pradesh. I like pop music, coding, sketching and shooting photos. Sometimes I write technical blogs.
+import profile from "../images/about.png";
 
-This is the spot for finding my projects I share with the world 😆.</a>
+const AnimatedText = () => {
+  const [words, setWords] = useState([]);
+  const [index, setIndex] = useState(0);
 
+  useEffect(() => {
+    const text = "hi i am amit kumar";
+    const wordArray = text.split(" ");
+
+    const interval = setInterval(() => {
+      if (index < wordArray.length) {
+        setWords(prevWords => [...prevWords, wordArray[index]]);
+        setIndex(prevIndex => prevIndex + 1);
+      } else {
+        clearInterval(interval);
+      }
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, [index]);
+
+  return (
+    <div>
+      {words.map((word, index) => (
+        <span key={index}>
+          {word}{' '}
+        </span>
+      ))}
+    </div>
+  );
+};
+
+function Home() {
+  return (
+    <div className="homecontainer background-image">
+      <div className="homeleft">
+        <div className="profile-circle">
+          <img src={profile} alt="" />
         </div>
-        </div>
-    )
+      </div>
+      <div className="homeright">
+        <h1 className='name'>" Hello!, i'm "
+          <span>Amit Kumar</span>
+          " A web Developer "
+          <AnimatedText />
+        </h1>
+        {/* Add additional content here */}
+      </div>
+    </div>
+  )
 }
+
 export default Home;
